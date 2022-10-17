b2f58aae704b2b35c68e4d96090297d1cd996684
Eunseo Choi <yses9296@gmail.com>
2022-10-17 15:57:55 +0900

    Vue 6-2

--- 6.로또추첨기/LottoGenerator.vue
+++ 6.로또추첨기/LottoGenerator.vue
@@ -15,21 +15,25 @@
 </template>
 <script>
     function getWinNumbers(){
-        const candidate = Array(45).fill().map( (v, i) => i + 1); //
+        const candidate = Array(45).fill().map( (v, i) => i + 1); // 1,2,3, ...44, 45 값을 배열에 저장
         const shuffle = [];
         while(candidate.length > 0){
             shuffle.push( candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0] );
-        }
+        } //배열을 섞어서 shuffle 배열에 넣는 과정
 
-        const bonusNumber = shuffle[shuffle.length - 1];
+        const bonusNumber = shuffle[shuffle.length - 1]; //shuffle의 맨 마지막 숫자
         const winNumbers = shuffle.slice(0, 6).sort( (p, c) => p - c);
 
         return [...winNumbers, bonusNumber];
     }
     export default {
+        components: {
+            'lotto-ball': LottoBall //import 하기
+        },
         data(){
             return {
-                winBalls: [],
+                winNumbers: getWinNumbers(), //미리 숫자를 다 뽑아놓고
+                winBalls: [], //시각적인 효과를 주기 위해, 하나씩 추가하여 하나씩 나타나도록
                 bonus: null,
                 redo: false,
             }
